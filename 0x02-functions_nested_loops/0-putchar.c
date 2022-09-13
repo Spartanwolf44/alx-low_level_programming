d6114026f3cbcdaa322c2fb49d5b6435685d90ab
Spartanwolf44 <dubemkizito5@gmail.com>
2022-09-13 16:03:24 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,43 +1,22 @@
-#include <stdio.h>
 
+#include "main.h"
 
 /**
- * main - entry point
+ * main - Entry point
  *
- * Return: always returns 0
+ * Return: Always returns 0
  *
  */
-
 int main(void)
-
 {
-	int i;
-	
-	int j;
-
-	for (i = 0; i <= 99; i++)
-	{
-		for (j = i + 1; j <= 99; j++)
-		{
-			putchar(i / 10 + '0');
-			
-			putchar(i % 10 + '0');
-			
-			putchar(' ');
-			
-			putchar(j / 10 + '0');
-			
-			putchar(j % 10 + '0');
-
-			if (!(i == 98 && j == 99))
-			{
-				putchar(',');
-				
-				putchar(' ');
-			}
-		}
-	}
-	putchar('\n');
-	
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
