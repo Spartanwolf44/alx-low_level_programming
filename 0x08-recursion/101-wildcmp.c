17d3e51c34b636be17df11823bfb7e94f0633a58
Spartanwolf44 <dubemkizito5@gmail.com>
2022-09-28 16:40:56 -0700

    task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -7,7 +7,7 @@
  * @s1: the normal string
  * @s2: the special string containing "*"
  *
-0 * Return: 1 if identical, else 0
+ * Return: 1 if identical, else 0
  */
 int wildcmp(char *s1, char *s2)
 {
@@ -24,5 +24,4 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 
 	return (0);
-}
 }
