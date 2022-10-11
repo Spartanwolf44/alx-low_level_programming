f411924cb0e3f05c87546e2dcc702d8758a7cae4
Spartanwolf44 <dubemkizito5@gmail.com>
2022-10-11 09:08:09 -0700

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,4 @@
 #ifndef DOG_H
-
 #define DOG_H
 
 /**
@@ -19,4 +18,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif/
+#endif
